98ddeaced01cb4d2c49938111ffe0e433bd013ba
unknown <mattcmej@gmail.com>
2020-05-19 14:11:54 -0700

    Remove extraneous comments from App.js.

--- src/App.js
+++ src/App.js
@@ -2,35 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Components
 import Testbox from './Components/Testbox';
 import PageHeader from './Components/PageHeader';
 
 // Needed for react-bootstrap to work.
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Jumbotron from 'react-bootstrap/Jumbotron';
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 class App extends Component {
 
