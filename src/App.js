9bc752699928808cca1686f44f090e16da7f47c0
unknown <mattcmej@gmail.com>
2020-05-18 18:52:41 -0700

    Install react-bootstrap and test jumbotron functionality.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import './App.css';
 
 import Testbox from './Components/Testbox';
 
+// Needed for react-bootstrap to work.
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
+
 // function App() {
 //   return (
 //     <div className="App">
@@ -30,10 +36,11 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Testbox 
+        {/* <Testbox 
           testboxClass='testbox'
           testboxText='Hello World!'
-        />
+        /> */}
+        <Jumbotron>Hello World</Jumbotron>
       </div>
     )
   }
